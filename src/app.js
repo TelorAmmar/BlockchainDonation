2cc7fd99ef1e2064b92d70b7dc3ef9d1322be9a8
TelorAmmar <hafidzammar3@gmail.com>
2025-06-04 00:45:48 +0700

    login

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/login.html'));
+});
 
 // Route auth
 const authRoutes = require("./routes/auth.routes");
